a428d5d13a359fa6274e5f6717ae94483ba8046e
MinexAutomation <minex.automation@gmail.com>
2019-06-14 15:25:49 -0400

    Initial commit of source-stub.

---
+++ source/R5T.NetStandard.StringSubstitution.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.NetStandard.StringSubstitution.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.NetStandard.StringSubstitution/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.NetStandard.StringSubstitution
+{
+    public class Class1
+    {
+    }
+}
