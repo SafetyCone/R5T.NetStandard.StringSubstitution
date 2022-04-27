9014774cdfd2ba20a2f2f0c0f4b5d96ca9e6ed77
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:05 -0700

    Interim changes.

---
+++ source/R5T.NetStandard.StringSubstitution/Code/Bases/Extensions/IServiceActionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.T0062;
+using R5T.T0063;
+
+
+namespace R5T.NetStandard.StringSubstitution
+{
+    public static class IServiceActionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="PercentStringSubstitutionScheme"/> implementation of <see cref="IStringSubstitutionScheme"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IStringSubstitutionScheme> AddPercentStringSubstitutionSchemeAction(this IServiceAction _)
+        {
+            var serviceAction = _.New<IStringSubstitutionScheme>(services => services.AddPercentStringSubstitutionScheme());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.NetStandard.StringSubstitution/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.T0063;
+
+
+namespace R5T.NetStandard.StringSubstitution
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="PercentStringSubstitutionScheme"/> implementation of <see cref="IStringSubstitutionScheme"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddPercentStringSubstitutionScheme(this IServiceCollection services)
+        {
+            services.AddSingleton<IStringSubstitutionScheme, PercentStringSubstitutionScheme>();
+
+            return services;
+        }
+    }
+}
